359210b3cc815e627dc89dd8ae9bf33019e720c8
Your Name <tayfun@mgssoftware.net>
2024-11-22 15:15:51 +0300

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger dokümantasyonu oluşturuluyor
+  const config = new DocumentBuilder()
+    .setTitle('Hekazagonal Mimari') // API başlığı
+    .setDescription('Hekazagonal Mimari API Dokümantasyonu') // API açıklaması
+    .setVersion('1.0') // API versiyonu
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Swagger'ı belirli bir endpoint'le açıyoruz
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
